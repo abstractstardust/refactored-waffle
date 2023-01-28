b81d4568637cca1a11d591c173c0cd17f5499538
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2023-01-28 18:02:53 -0500

    add json res

--- function/main.py
+++ function/main.py
@@ -1,9 +1,19 @@
 import datetime
 import socket
 import ssl
+import json
 
-def ssl_cert_check_handler(event, context):
-    hostname = "fearless.tech"
+
+def ssl_cert_check_handler():
+    # bad hosts
+    # hostname = "expired.badssl.com"
+    # hostname = "wrong.host.badssl.com"
+    # hostname = "self-signed.badssl.com"
+    # hostname = "untrusted-root.badssl.com"
+    # hostname = "revoked.badssl.com"
+
+    # good hosts
+    hostname = "https-everywhere.badssl.com"
 
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
 
@@ -12,11 +22,38 @@ def ssl_cert_check_handler(event, context):
         socket.socket(socket.AF_INET),
         server_hostname=hostname,
     )
+
     # 3 second timeout because Lambda has runtime limitations
     conn.settimeout(3.0)
 
-    conn.connect((hostname, 443))
-    ssl_info = conn.getpeercert()
-    expires = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+    try:
+        # connect to a server
+        conn.connect((hostname, 443))
+    except Exception as HostConnectionError:
+        res = {
+            "domain": hostname,
+            "valid": (False, HostConnectionError.args[1])
+        }
+    else:
+        try:
+            ssl_info = conn.getpeercert()
 
-    return expires > datetime.datetime.utcnow()
+            expires = datetime.datetime.strptime(
+                ssl_info['notAfter'], ssl_date_fmt)
+            valid = expires > datetime.datetime.utcnow()
+            time_until_expiration = expires - datetime.datetime.utcnow()
+
+            res = {
+                "domain": hostname,
+                "valid": valid,
+                "expires": expires.isoformat(),
+                "days until expiration": time_until_expiration.days,
+            }
+        except Exception as e:
+            print("ssl_info error: %s" % e)
+
+    print(json.dumps(res, indent=2))
+    return json.dumps(res)
+
+
+ssl_cert_check_handler()
