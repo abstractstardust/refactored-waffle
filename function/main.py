46f3de5d0861227ec5b77e12b74a179b3f83c317
abstractstardust <123878221+abstractstardust@users.noreply.github.com>
2023-01-29 15:33:22 -0500

    Revert "Create AWS API Gateway Integration and Modify AWS Lambda Function"

--- function/main.py
+++ function/main.py
@@ -1,48 +1,22 @@
 import datetime
 import socket
 import ssl
-import json
-
 
 def ssl_cert_check_handler(event, context):
-    domain = event["pathParameters"]["domain"]
+    hostname = "fearless.tech"
 
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
 
     context = ssl.create_default_context()
     conn = context.wrap_socket(
         socket.socket(socket.AF_INET),
-        server_hostname=domain,
+        server_hostname=hostname,
     )
-
     # 3 second timeout because Lambda has runtime limitations
     conn.settimeout(3.0)
 
-    res = {}
+    conn.connect((hostname, 443))
+    ssl_info = conn.getpeercert()
+    expires = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
 
-    try:
-        # connect to a server
-        conn.connect((domain, 443))
-    except Exception as HostConnectionError:
-        res['domain'] = domain
-        res['is_valid'] = False
-        res['issue'] = HostConnectionError.args[1]
-    else:
-        try:
-            ssl_info = conn.getpeercert()
-
-            expires = datetime.datetime.strptime(
-                ssl_info['notAfter'], ssl_date_fmt)
-            valid = expires > datetime.datetime.utcnow()
-            time_until_expiration = expires - datetime.datetime.utcnow()
-
-            res['domain'] = domain
-            res['is_valid'] = valid
-            res['expires'] = expires.isoformat()
-            res['days until expiration'] = time_until_expiration.days
-
-        except Exception as GetPeerCertError:
-            return ("ERROR: ", GetPeerCertError)
-
-    return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'
-                                           }, 'body': json.dumps(res)}
+    return expires > datetime.datetime.utcnow()
--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "ssl_lambda" {
   function_name    = "ssl_checker"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "main.ssl_cert_check_handler"
-  runtime          = "python3.9"
+  runtime = "python3.9"
   source_code_hash = data.archive_file.ssl_lambda.output_base64sha256
   depends_on = [
     data.archive_file.ssl_lambda
